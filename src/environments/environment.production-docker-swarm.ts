a7c2e4f998b3defb8dcfca7e4202e24d273adc33
Arjay Fuentes <arjay@gmail.com>
2025-03-25 15:32:55 +0100

    production docker swarm

---
+++ src/environments/environment.production-docker-swarm.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: true,
+    caching: true,
+    auth: {
+        issuer: 'https://extraxxx.xxxxx', // Identity Provider URL
+        clientId: 'xxxxx', // Client ID
+        postLogoutRedirectUri: 'http://127.0.0.1:30085/', // Redirect URL after logout
+        redirectUri: window.location.origin + "/login/callback",
+        silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+        scope: 'openid profile logistack_apps authorization',
+        requireHttps: false,
+        disableAtHashCheck: true,
+        responseType: 'id_token token',
+        preserveRequestedRoute: true,
+        oidc: true,
+        showDebugInformation: false,  // Disable debug info in production
+        clearHashAfterLogin: true,
+        strictDiscoveryDocumentValidation: false,
+    },
+    apiUrl: process.env['API_URL'] || 'http://gateway-service:8222',  // Default to the gateway service DNS
+    countryUrl: 'https://api.countrystatecity.in/v1',
+    requireHttps: false
+};
