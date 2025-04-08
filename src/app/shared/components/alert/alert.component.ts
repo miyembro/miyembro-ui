4e27b27aca1770cf27cfa72ef4d8361d6e7d4655
Arjay Fuentes <arjay@gmail.com>
2025-04-08 14:19:42 +0200

    refactored p-toast

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -1,21 +1,26 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AlertService } from '../../../core/services/alert.service';
-import { GLOBAL_ALERTS } from '../../../core/constants/global-alerts';
 import { Toast } from 'primeng/toast';
+import { CommonModule } from '@angular/common';
+import { ToastPositionType } from 'primeng/toast'; // Import the correct type
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
-  imports: [Toast]
+  imports: [Toast, CommonModule]
 })
 export class AlertComponent implements OnInit, OnDestroy {
   
   @Input() alertKey?: string;
+  isMobile = false;
+  mobileBreakpoint = 768;
+  toastClasses = '';
+  toastPosition: ToastPositionType = 'bottom-center'; 
 
   private unsubscribe: Subject<any> = new Subject();
 
@@ -25,48 +30,47 @@ export class AlertComponent implements OnInit, OnDestroy {
     private router: Router
   ) {}
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
   ngOnInit(): void {
-    // Assign a default value if alertKey is undefined
     this.alertKey = this.alertKey || this.router.url || 'default-alert-key';
-
-    this.alertService
-      .getAlerts()
-      .pipe(takeUntil(this.unsubscribe))
-      .subscribe((alertOptions) => {
-        // if (!alertOptions) {
-        //   return;
-        // }
-
-        // const key = alertOptions.key || this.alertKey;
-
-        // // // multiple component instances, each only listens for its own key
-        // // if (key !== this.alertKey) {
-        // //   return;
-        // // }
-
-        // // clear request
-        // if (alertOptions.clear) {
-        //   this.messageService.clear(key as string);
-        //   return;
-        // }
-
-        // // don't include global alerts
-        // if (!alertOptions.includeGlobalAlerts && alertOptions.summary && GLOBAL_ALERTS.includes(alertOptions.summary)) {
-        //   return;
-        // }
-
-        // regular message
-        this.messageService.add({
-          severity: alertOptions.type || 'info',
-          summary: alertOptions.summary,
-          detail: alertOptions.detail,
-          life: 2000
-        });
-      });
+    this.setupAlertSubscription();
   }
 
   ngOnDestroy(): void {
     this.unsubscribe.next(0);
     this.unsubscribe.complete();
   }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth < this.mobileBreakpoint;
+    this.toastPosition = this.isMobile ? 'bottom-center' : 'top-right';
+    this.updateToastClasses();
+  }
+
+  private setupAlertSubscription() {
+    this.checkScreenSize();
+    this.alertService
+    .getAlerts()
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe((alertOptions) => {
+      this.messageService.add({
+        severity: alertOptions.type || 'info',
+        summary: alertOptions.summary,
+        detail: alertOptions.detail,
+        life: 4000,
+      });
+    });
+  }
+
+  private updateToastClasses() {
+    const baseClasses = 'text-xs sm:text-xs md:text-sm w-[80vw] sm:w-[80vw] md:w-[25vw]';
+    this.toastClasses = this.isMobile 
+      ? `p-toast-text ${baseClasses}` 
+      : baseClasses;
+  }
+
 }
